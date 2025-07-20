747df80e1d5c4dfed8cc8573af73eba5d7fd7662
777aker <777aker@gmail.com>
2025-07-20 15:41:26 -0600

    trying more configs for new issues

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/scalability/',
     server: {
+        hmr: {
+            host: "localhost",
+            protocol: "ws",
+        },
         allowedHosts: [
             '.planetinkgames.fun',
         ],
