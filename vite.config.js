3ebe8e17c193e3ac53092fb5ffeca17bb2a330a5
777aker <777aker@gmail.com>
2025-07-20 15:46:44 -0600

    fix websocket proxy issue

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/scalability/',
     server: {
+        port: "5173",
         hmr: {
             host: "localhost",
             protocol: "ws",
+            port: "5174",
         },
         allowedHosts: [
             '.planetinkgames.fun',
