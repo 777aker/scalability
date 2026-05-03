65b8d15ef1cb16754b9dca3cae669dc4d1d4525e
Kelley <777aker@gmail.com>
2026-05-03 09:24:19 -0600

    snake improvements, selecting and closing windows

--- scenes/snake/snake_manager.gd
+++ scenes/snake/snake_manager.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 var body: Array[Node2D] = []
 const body_segment = preload("res://scenes/snake/snake_segment.tscn")
-var head_pos: Vector2 = Vector2(255, 250) # 255 so doesn't collide with edges of screen
+var head_pos: Vector2 = Vector2(205, 200) # 255 so doesn't collide with edges of screen
 var direction: Vector2 = Vector2(0, -1);
 var current_length: int = 1
 const GRID_SIZE = 20
 const apple = preload("res://scenes/apple.tscn")
+@onready var PARENT_WINDOW = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +17,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if not PARENT_WINDOW.selected_window:
+		return
 	if Input.is_action_pressed("down"):
 		direction = Vector2(0, 1)
 	if Input.is_action_pressed("left"):
@@ -30,7 +33,7 @@ func spawn_segment() -> void:
 	var new_segment = body_segment.instantiate()
 	body.push_back(new_segment)
 	head_pos += direction * GRID_SIZE
-	if head_pos.x < 0 or head_pos.y < 36 or head_pos.x > 500 or head_pos.y > 520:
+	if head_pos.x <= 20 or head_pos.y <= 40 or head_pos.x > 400 or head_pos.y > 420:
 		game_over()
 		return
 	new_segment.position = head_pos
@@ -42,8 +45,8 @@ func spawn_segment() -> void:
 
 
 func spawn_apple() -> void:
-	var apple_pos_x = randi_range(0, 480 / GRID_SIZE) * GRID_SIZE + 15 # has a little offset from edge of screen
-	var apple_pos_y = randi_range(0, 480 / GRID_SIZE) * GRID_SIZE + 50 # has a little offset from title bar
+	var apple_pos_x = randi_range(1, 19) * GRID_SIZE + 5 # has a little offset from edge of screen
+	var apple_pos_y = randi_range(3, 19) * GRID_SIZE # has a little offset from title bar
 	var new_apple = apple.instantiate()
 	new_apple.position = Vector2(apple_pos_x, apple_pos_y)
 	add_child(new_apple)
@@ -51,16 +54,22 @@ func spawn_apple() -> void:
 
 func _on_segment_spawner_timeout() -> void:
 	spawn_segment()
-	
+
+
 func game_over() -> void:
 	for segment in body:
 		segment.queue_free()
 	body.clear()
 	current_length = 1
-	head_pos = Vector2(255, 250)
+	head_pos = Vector2(205, 200)
 	direction = Vector2(0, -1);
+	for child in get_children():
+		if child.is_in_group("apple"):
+			child.queue_free()
 	spawn_segment()
-	
+	spawn_apple()
+
+
 func area_entered_segment(area: Area2D) -> void:
 	if area.get_groups().has("snakebody"):
 		game_over()
--- scenes/window/window.gd
+++ scenes/window/window.gd
@@ -1,6 +1,7 @@
 extends Panel
 
 @export var window_size: Vector2
+@export var starting_position: Vector2
 @onready var title_bar_node = $TitleBar
 @onready var window_title_node = $TitleBar/TitleBarControl/TitleContainer/WindowTitle
 @export var window_title: String
@@ -8,15 +9,24 @@ extends Panel
 var mouse_inside: bool = false
 var mouse_title: bool = false
 var dragging: bool = false
+var selected_window: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	custom_minimum_size = window_size
 	window_title_node.text = window_title
+	if starting_position:
+		print("has position")
+		position = starting_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if mouse_inside and Input.is_action_pressed("mouseleft"):
+		selected_window = true
+	elif Input.is_action_pressed("mouseleft"):
+		selected_window = false
+	
 	if (mouse_title or dragging) and Input.is_action_pressed("mouseleft"):
 		dragging = true
 		position = get_global_mouse_position() - Vector2(get_global_rect().size.x / 2.0, title_bar_node.get_global_rect().size.y / 2.0)
@@ -38,3 +48,7 @@ func _on_title_bar_control_mouse_entered() -> void:
 
 func _on_title_bar_control_mouse_exited() -> void:
 	mouse_title = false
+
+
+func _on_close_pressed() -> void:
+	queue_free()
